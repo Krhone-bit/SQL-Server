a1a057f14bf2ab4a216b6f03a706ffbb2c04155f
Krhone-bit <alvaroupeu@gmail.com>
2024-08-03 12:54:06 -0500

    add having count

--- 23_BACKUP.sql
+++ 23_BACKUP.sql
@@ -1,3 +1,17 @@
 BACKUP DATABASE dbCompuCenter
-TO DISK = 'C:\Users\alvar\Downloads\Backup'
+TO DISK = 'C:\Backup\dbCompuCenter20240802.bak'
 WITH NO_COMPRESSION, NAME='dbCompuCenter20240802.bak'
+
+declare @fecha char(12)
+declare @path varchar(100)
+declare @name varchar(20)
+
+--print convert(char(5),getdate(),108)
+set @fecha = convert(char(8),getdate(),112) + replace(convert(char(5),getdate(),108),':','')
+set @path = 'C:\Backup\dbCompuCenter'+@fecha+'.bak'
+set @name = 'dbCompuCenter'+@fecha
+
+
+BACKUP DATABASE dbCompuCenter
+TO DISK = @path
+WITH NO_COMPRESSION, NAME=@name
---
+++ 24_HAVING_COUNT.sql
@@ -0,0 +1,15 @@
+-- Cantidad De Clientes Por Fecha
+SELECT FORMAT(V.FechaVenta, 'MMMM') FechaVenta, C.Nombre, COUNT(*) CantidadClientes FROM 
+Venta V
+JOIN Cliente C ON V.ClienteID = C.ClienteID
+Group By V.FechaVenta, C.Nombre
+HAVING COUNT(C.Nombre)>1;
+
+-- Cantidad De Clientes Por Mes
+SELECT T.FechaVenta, SUM(CantidadClientes) TotalCatidadDeClientes FROM (
+SELECT FORMAT(V.FechaVenta, 'MMMM') FechaVenta, C.Nombre, COUNT(*) CantidadClientes FROM 
+Venta V
+JOIN Cliente C ON V.ClienteID = C.ClienteID
+Group By V.FechaVenta, C.Nombre
+HAVING COUNT(C.Nombre)>1)
+T GROUP BY FechaVenta;
